a875dcd1e5992ed288c4c797841a0e045bd4f33f
sranmanpreet <manna1661@gmail.com>
2020-06-13 16:25:45 +0530

    Sanity

--- backup_customization/backup_custom-woo-addon-uploads/assets/js/wau_upload_cart_script.js
+++ backup_customization/backup_custom-woo-addon-uploads/assets/js/wau_upload_cart_script.js
@@ -15,22 +15,22 @@ $j(document).ready(function() {
 function renderPictureCart_ExclusiveForYouProduct() {
     "use strict"
 
-    var $productThumbnail = $j('td');
-    var $productName = $j('td.product-name a');
-    console.log($productName);
+    /*     var $productThumbnail = $j('td');
+        var $productName = $j('td.product-name a');
+        console.log($productName);
 
-    console.log($j('.product-name a'));
+        console.log($j('.product-name a'));
 
-    $j.each($productName, function(index, value) {
-        console.log(index);
-        console.log(value);
-        if ("Exclusive For You" == value.text()) {
-            alert(" i am here");
-            $productThumbnail.hide();
-        } else {
-            alert("i am in else");
-        }
-    })
+        $j.each($productName, function(index, value) {
+            console.log(index);
+            console.log(value);
+            if ("Exclusive For You" == value.text()) {
+                alert(" i am here");
+                $productThumbnail.hide();
+            } else {
+                alert("i am in else");
+            }
+        }) */
 
 
 }
